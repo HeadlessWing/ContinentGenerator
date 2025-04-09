1afefb59b1a978f20ab67c9ca72c13bb8004a6da
HeadlessWing <wingcue@gmail.com>
2025-04-08 21:24:45 -0700

    created base files and got a window up and running

---
+++ main.py
@@ -0,0 +1,7 @@
+from window import *
+from plate import *
+def main():
+    win = Window(1000, 1000)
+    win.wait_for_close()
+if __name__ == "__main__":
+    main()
---
+++ window.py
@@ -0,0 +1,29 @@
+import tkinter as tk
+import time
+import random
+
+class Window:
+    def __init__(self, width, height):
+        self.__root = tk.Tk()
+        self.__root.title = ("Continent Generator")
+        self.parameter_frame = tk.Frame(self.__root)
+        self.parameter_frame.pack(side=tk.TOP, fill=tk.X, pady=5)
+        self.__canvas = tk.Canvas(self.__root, bg="white", height=height, width=width)
+        self.__canvas.pack(fill = tk.BOTH, expand = 1)
+        self.__running = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self):
+        self.__running = True
+        while self.__running == True:
+            self.redraw()
+    
+    def close(self):
+        self.__running = False
+    
+    
